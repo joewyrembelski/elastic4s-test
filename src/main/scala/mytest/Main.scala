d46cfcaf38c109468bf02856a6891c17d9bd4861
Joe Wyrembelski <joe@wyrembelski.com>
2014-03-28 05:31:53 -0400

    Formatting and killing unused case classes

--- src/main/scala/mytest/Main.scala
+++ src/main/scala/mytest/Main.scala
@@ -32,6 +32,7 @@ object Main extends App {
 
 
   if (numMonths > 0) {
+
     /*
       Step #1: Clear index and install mappings
     */
--- src/main/scala/mytest/series-definitions.scala
+++ src/main/scala/mytest/series-definitions.scala
@@ -2,8 +2,3 @@ package mytest
 
 
 case class MultiNumberDataSeries(label: String, data: Seq[Seq[AnyVal]])
-
-case class MultiLongDataSeries(label: String, data: Seq[Seq[Long]])
-
-case class MultiDecimalDataSeries(label: String, data: Seq[(Long, BigDecimal)])
-
